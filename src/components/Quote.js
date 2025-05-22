009759c977f458b8378dda0c362365dac7f2e549
Amrit Aujla <amritpalaujla204@gmail.com>
2025-05-21 23:34:36 -0700

    new Quote Button added

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -7,6 +7,7 @@ const Quote = () => {
   //here we are creating state vatiables which we can use later to get a quote
   const [quotes, setQuotes] = useState([]);
   const [idx, setIdx] = useState(0);
+
   // here we are fetching quotes from a link or better known as an API endpoint
   // I feel smart whenever using the word API + 1000 aura points
 
@@ -26,6 +27,10 @@ const Quote = () => {
       .catch((err) => console.error("Fetch error,", err));
   }, []);
 
+  const newQuote = () => {
+    setIdx(Math.floor(Math.random() * quotes.length));
+  };
+
   //let idx = Math.floor(Math.random() * 29);
 
   // this will prevent runtime
@@ -40,9 +45,13 @@ const Quote = () => {
           </span>
           {quotes[idx].quote}
         </p>
+
         <p id="author">
           <i>~ {quotes[idx].author}</i>
         </p>
+        <div id="Btn">
+          <button onClick={newQuote}>New Quote</button>
+        </div>
       </div>
     );
   }
